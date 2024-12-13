e1d1113fb230acdb54a911c3b73df27af0a86a97
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2024-12-14 05:07:14 +0800

    testGangLie

--- standard/test/init.lua
+++ standard/test/init.lua
@@ -2,7 +2,7 @@ TestStandard = { setup = InitRoom, tearDown = ClearRoom }
 
 function TestStandard:testJianxiong()
   local room = LRoom
-  local me, comp2 = room.players[1], room.players[2]
+  local me, comp2 = room.players[1], room.players[2] ---@type ServerPlayer
   RunInRoom(function() room:handleAddLoseSkills(me, "jianxiong") end)
 
   local slash = Fk:getCardById(1)
@@ -14,5 +14,26 @@ function TestStandard:testJianxiong()
       card = slash,
     }
   end)
+  -- p(me:toJsonObject())
+  lu.assertEquals(me:getCardIds("h")[1], 1)
+end
+
+function TestStandard:testGangLie()
+  local room = LRoom
+  local me, comp2 = room.players[1], room.players[2] ---@type ServerPlayer
+  RunInRoom(function() room:handleAddLoseSkills(me, "ganglie") end)
+
+  local slash = Fk:getCardById(1)
+  SetNextReplies(me, { "__cancel", "1" })
+  RunInRoom(function()
+    room:drawCards(comp2, 2)
+    room:moveCardTo(2, Card.DrawPile)
+    room:useCard{
+      from = comp2.id,
+      tos = { { me.id } },
+      card = slash,
+    }
+  end)
   p(me:toJsonObject())
+  p(comp2:toJsonObject()) -- TODO: 问题来了：我该如何让AI觉得我必须绝不取消呢？
 end
