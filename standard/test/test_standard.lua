93b2fc46c6a256d114b17181c7c5ca8dc454f5ef
超级妖梦厨 <12796194+youmuKon-supreme@user.noreply.gitee.com>
2024-12-20 16:50:58 +0800

    马术test

--- standard/test/test_standard.lua
+++ standard/test/test_standard.lua
@@ -90,6 +90,23 @@ function TestStandard:testGangLie()
   end)
   lu.assertEquals(comp2.hp, origin_hp)
   lu.assertEquals(#comp2:getCardIds("h"), 0)
+
+  -- 第三段：测试我发动刚烈，判定判红桃
+  origin_hp = comp2.hp
+  SetNextReplies(me, { "__cancel", "1" })
+  SetNextReplies(comp2, { "__cancel" })
+  RunInRoom(function()
+    room:obtainCard(comp2, { 3, 4 })
+
+    room:moveCardTo(24, Card.DrawPile) -- 控顶
+    room:useCard{
+      from = comp2.id,
+      tos = { { me.id } },
+      card = slash,
+    }
+  end)
+  lu.assertEquals(comp2.hp, origin_hp)
+  lu.assertEquals(#comp2:getCardIds("h"), 2)
 end
 
 function TestStandard:testLuoYi()
@@ -98,7 +115,7 @@ function TestStandard:testLuoYi()
   RunInRoom(function()
     room:handleAddLoseSkills(me, "luoyi")
   end)
-  -- local slash = Fk:getCardById(1)
+  local slash = Fk:getCardById(1)
   SetNextReplies(me, { "1", json.encode {
     card = 1,
     targets = { comp2.id }
@@ -113,4 +130,30 @@ function TestStandard:testLuoYi()
   -- p(me:getCardIds("h"))
   lu.assertEquals(#me:getCardIds("h"), 1)
   lu.assertEquals(comp2.hp, origin_hp - 2)
+
+  -- 测标记持续时间
+  origin_hp = comp2.hp
+  RunInRoom(function()
+    room:useCard{
+      from = me.id,
+      tos = { { comp2.id } },
+      card = slash,
+    }
+  end)
+  lu.assertEquals(comp2.hp, origin_hp - 1)
+end
+
+function TestStandard:testMashu()
+  local room = LRoom ---@type Room
+  local me = room.players[1] ---@type ServerPlayer
+
+  local origin = table.map(room:getOtherPlayers(me), function(other) return me:distanceTo(other) end)
+
+  RunInRoom(function()
+    room:handleAddLoseSkills(me, "mashu")
+  end)
+
+  for i, other in ipairs(room:getOtherPlayers(me)) do
+    lu.assertEquals(me:distanceTo(other), math.max(origin[i] - 1, 1))
+  end
 end
