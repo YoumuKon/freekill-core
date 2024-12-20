a582ab1a9ec2f215136547294cc5906f9ca733fe
超级妖梦厨 <12796194+youmuKon-supreme@user.noreply.gitee.com>
2024-12-20 16:37:38 +0800

    裸衣test

--- standard/test/test_standard.lua
+++ standard/test/test_standard.lua
@@ -18,6 +18,36 @@ function TestStandard:testJianxiong()
   lu.assertEquals(me:getCardIds("h")[1], 1)
 end
 
+function TestStandard:testFanKui()
+  local room = LRoom
+  local me, comp2 = room.players[1], room.players[2] ---@type ServerPlayer, ServerPlayer
+  RunInRoom(function() room:handleAddLoseSkills(me, "fankui") end)
+
+  -- 空牌的情况
+  local slash = Fk:getCardById(1)
+  SetNextReplies(me, { "__cancel" })
+  RunInRoom(function()
+    room:useCard{
+      from = comp2.id,
+      tos = { { me.id } },
+      card = slash,
+    }
+  end)
+  lu.assertEquals(#me:getCardIds("h"), 0)
+
+  -- 有牌的情况
+  SetNextReplies(me, { "__cancel", "1", "3" })
+  RunInRoom(function()
+    room:obtainCard(comp2, { 3 })
+    room:useCard{
+      from = comp2.id,
+      tos = { { me.id } },
+      card = slash,
+    }
+  end)
+  lu.assertEquals(me:getCardIds("h")[1], 3)
+end
+
 function TestStandard:testGangLie()
   local room = LRoom ---@type Room
   local me, comp2 = room.players[1], room.players[2] ---@type ServerPlayer, ServerPlayer
@@ -47,8 +77,8 @@ function TestStandard:testGangLie()
   origin_hp = comp2.hp
   SetNextReplies(me, { "__cancel", "1" })
   SetNextReplies(comp2, { json.encode {
-    targets = {},
-    card = { skill = "discard_skill", subcards = { 3, 4 } }
+    card = { skill = "discard_skill", subcards = { 3, 4 } },
+    targets = {}
   } })
   RunInRoom(function()
     room:moveCardTo(2, Card.DrawPile) -- 再控顶
@@ -62,32 +92,25 @@ function TestStandard:testGangLie()
   lu.assertEquals(#comp2:getCardIds("h"), 0)
 end
 
-function TestStandard:testFanKui()
-  local room = LRoom
+function TestStandard:testLuoYi()
+  local room = LRoom ---@type Room
   local me, comp2 = room.players[1], room.players[2] ---@type ServerPlayer, ServerPlayer
-  RunInRoom(function() room:handleAddLoseSkills(me, "fankui") end)
-
-  -- 空牌的情况
-  local slash = Fk:getCardById(1)
-  SetNextReplies(me, { "__cancel" })
   RunInRoom(function()
-    room:useCard{
-      from = comp2.id,
-      tos = { { me.id } },
-      card = slash,
-    }
+    room:handleAddLoseSkills(me, "luoyi")
   end)
-  lu.assertEquals(#me:getCardIds("h"), 0)
+  -- local slash = Fk:getCardById(1)
+  SetNextReplies(me, { "1", json.encode {
+    card = 1,
+    targets = { comp2.id }
+  } })
+  SetNextReplies(comp2, { "__cancel" })
 
-  -- 有牌的情况
-  SetNextReplies(me, { "__cancel", "1", "3" })
+  local origin_hp = comp2.hp
   RunInRoom(function()
-    room:obtainCard(comp2, { 3 })
-    room:useCard{
-      from = comp2.id,
-      tos = { { me.id } },
-      card = slash,
-    }
+    room:obtainCard(me, 1)
+    GameEvent.Turn:create(me):exec()
   end)
-  lu.assertEquals(me:getCardIds("h")[1], 3)
+  -- p(me:getCardIds("h"))
+  lu.assertEquals(#me:getCardIds("h"), 1)
+  lu.assertEquals(comp2.hp, origin_hp - 2)
 end
