079ee8dac831356c2c3e8b40e1103d927e1f3f1d
notify <notify-ctrl@qq.com>
2024-12-23 19:10:48 +0800

    浅搬几个data……算了先存档

--- lua/core/events/hp.lua
+++ lua/core/events/hp.lua
@@ -1,3 +1,57 @@
+
+--- HpChangedData 描述和一次体力变化有关的数据
+---@class HpChangedDataSpec
+---@field public num integer @ 体力变化量，可能是正数或者负数
+---@field public shield_lost? integer
+---@field public reason string @ 体力变化原因
+---@field public skillName string @ 引起体力变化的技能名
+---@field public damageEvent? DamageStruct @ 引起这次体力变化的伤害数据
+---@field public preventDying? boolean @ 是否阻止本次体力变更流程引发濒死流程
+
+---@class HpChangedData: HpChangedDataSpec, Object
+HpChangedData = class("HpChangedData")
+
+--- HpLostData 描述跟失去体力有关的数据
+---@class HpLostDataSpec
+---@field public num integer @ 失去体力的数值
+---@field public skillName string @ 导致这次失去的技能名
+
+--- MaxHpChangedData 描述跟体力上限变化有关的数据
+---@class MaxHpChangedDataSpec
+---@field public num integer @ 体力上限变化量，可能是正数或者负数
+
+--- DamageType 伤害的属性
+---@alias DamageType integer
+
+fk.NormalDamage = 1
+fk.ThunderDamage = 2
+fk.FireDamage = 3
+fk.IceDamage = 4
+
+--- DamageStruct 描述和伤害事件有关的数据。
+---@class DamageStructSpec
+---@field public from? ServerPlayer @ 伤害来源
+---@field public to ServerPlayer @ 伤害目标
+---@field public damage integer @ 伤害值
+---@field public card? Card @ 造成伤害的牌
+---@field public chain? boolean @ 伤害是否是铁索传导的伤害
+---@field public damageType? DamageType @ 伤害的属性
+---@field public skillName? string @ 造成本次伤害的技能名
+---@field public beginnerOfTheDamage? boolean @ 是否是本次铁索传导的起点
+---@field public by_user? boolean @ 是否由卡牌直接生效造成的伤害
+---@field public chain_table? ServerPlayer[] @ 铁索连环表
+
+---@class DamageStruct: DamageStructSpec, Object
+DamageStruct = class("DamageStruct")
+
+--- RecoverStruct 描述和回复体力有关的数据。
+---@class RecoverStructSpec
+---@field public who ServerPlayer @ 回复体力的角色
+---@field public num integer @ 回复值
+---@field public recoverBy? ServerPlayer @ 此次回复的回复来源
+---@field public skillName? string @ 因何种技能而回复
+---@field public card? Card @ 造成此次回复的卡牌
+
 ---@class HpChangedEvent: TriggerEvent
 ---@field data HpChangedData
 local HpChangedEvent = TriggerEvent:subclass("HpChangedEvent")
