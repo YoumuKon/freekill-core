80f49da71dd6f829418b8ef882a67b852f036e33
seven <786852516@qq.com>
2025-02-18 21:25:02 +0800

    修复

--- lua/server/events/movecard.lua
+++ lua/server/events/movecard.lua
@@ -457,6 +457,7 @@ end
 ---@param convert? boolean @ 是否可以替换装备（默认可以）
 ---@param proposer? ServerPlayer @ 操作者
 function MoveEventWrappers:moveCardIntoEquip(target, cards, skillName, convert, proposer)
+  ---@cast self Room
   convert = (convert == nil) and true or convert
   skillName = skillName or ""
   cards = type(cards) == "table" and cards or {cards}
@@ -467,17 +468,16 @@ function MoveEventWrappers:moveCardIntoEquip(target, cards, skillName, convert, 
     local card = Fk:getCardById(cardId)
     local from = self:getCardOwner(cardId)
     if target:canMoveCardIntoEquip(cardId, convert) then
-      if target:hasEmptyEquipSlot(card.sub_type) then
-        table.insert(moves,{ids = {cardId}, from = from, to = target, toArea = Card.PlayerEquip, moveReason = fk.ReasonPut,skillName = skillName,proposer = proposer})
-      else
+      if not target:hasEmptyEquipSlot(card.sub_type) then
         local existingEquip = target:getEquipments(card.sub_type)
         local throw = #existingEquip == 1 and existingEquip[1] or
         self:askToChooseCard(proposer or target, { target = target, flag = { card_data = { { Util.convertSubtypeAndEquipSlot(card.sub_type),existingEquip } } }, skill_name = "replaceEquip", prompt = "#replaceEquip" })
-        table.insert(moves, { ids = { throw }, from = target.id, toArea = Card.DiscardPile, moveReason = fk.ReasonPutIntoDiscardPile, skillName = skillName, proposer = proposerId })
-        table.insert(moves, { ids = { cardId }, from = fromId, to = target.id, toArea = Card.PlayerEquip, moveReason = fk.ReasonPut, skillName = skillName, proposer = proposerId })
+        table.insert(moves, { ids = { throw }, from = target, toArea = Card.DiscardPile, moveReason = fk.ReasonPutIntoDiscardPile,
+        skillName = skillName, proposer = proposer })
       end
+      table.insert(moves, { ids = { cardId }, from = from, to = target, toArea = Card.PlayerEquip, moveReason = fk.ReasonPut, skillName = skillName, proposer = proposer })
     else
-      table.insert(moves,{ ids = { cardId }, from = fromId, toArea = Card.DiscardPile, moveReason = fk.ReasonPutIntoDiscardPile,skillName = skillName })
+      table.insert(moves, { ids = { cardId }, from = from, toArea = Card.DiscardPile, moveReason = fk.ReasonPutIntoDiscardPile, skillName = skillName, proposer = proposer })
     end
   end
   self:moveCards(table.unpack(moves))
